10cce12dc9e9a6629b3ea63c22e68a08a9cfb733
Eric <ericcui@qq.com>
2024-09-30 13:24:21 +0800

    add sample code

---
+++ SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+#if WINDOWS
+            CommunityToolkit.WinUI.Controls.DockPanel dp = null;
+#endif
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
